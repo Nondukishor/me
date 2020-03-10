b0aa738bb2b2a1d6be76f7f7faeaba8d0dc0eaa1
Nondukishor <nipuchakraborty86@hotmail.com>
2020-03-10 15:16:29 +0600

    Updates

---
+++ precache-manifest.80b17aca70db669bd99606f61a583839.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b543b1e936a5b74b8dcb5f79549acb03",
+    "url": "/me/index.html"
+  },
+  {
+    "revision": "4d67a194b731016c9f24",
+    "url": "/me/static/js/2.6e5334e0.chunk.js"
+  },
+  {
+    "revision": "ac9fac6c8e755a8efecd01981adb88e6",
+    "url": "/me/static/js/2.6e5334e0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "398596652fd30006f6e4",
+    "url": "/me/static/js/main.4f142bfd.chunk.js"
+  },
+  {
+    "revision": "59de9076f02f5d974473",
+    "url": "/me/static/js/runtime-main.38d0503b.js"
+  },
+  {
+    "revision": "47ce6e77f039020ee2e76a10c1e988e9",
+    "url": "/me/static/media/React.47ce6e77.png"
+  },
+  {
+    "revision": "bfabb0d6088a59a5345cd9019927caa7",
+    "url": "/me/static/media/jquery.bfabb0d6.png"
+  },
+  {
+    "revision": "d52287738b55788ea70175d566102073",
+    "url": "/me/static/media/laravel.d5228773.svg"
+  },
+  {
+    "revision": "7930d4700e3f08ef71f2d43ab4d8ac71",
+    "url": "/me/static/media/me.7930d470.jpg"
+  },
+  {
+    "revision": "46318f0710da4390da47e48c903f54e0",
+    "url": "/me/static/media/mysql.46318f07.webp"
+  },
+  {
+    "revision": "1b29a538acb3efa07b2289d932b2daa9",
+    "url": "/me/static/media/php.1b29a538.png"
+  }
+]);
